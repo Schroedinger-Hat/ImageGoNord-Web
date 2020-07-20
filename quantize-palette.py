649fb9c5af693bc8810240a64e06ed1b4a8aaf45
Miki <miki.lombi@gmail.com>
2020-07-20 17:41:13 +0200

    add demo
    Add test image
    This demo use the latest pillow library

---
+++ quantize-palette.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+def quantizetopalette(silf, palette, dither=False):
+
+    silf.load()
+
+    palette.load()
+    if palette.mode != "P":
+        raise ValueError("bad mode for palette image")
+    if silf.mode != "RGB" and silf.mode != "L":
+        raise ValueError(
+            "only RGB or L mode images can be quantized to a palette"
+            )
+    im = silf.im.convert("P", 1 if dither else 0, palette.im)
+
+    # Altro metodo
+    silf.quantize(palette=palette)
+    
+    # retrocompatibilità per le 4.0 di pillow
+    try:
+        return silf._new(im)
+    except AttributeError:
+        return silf._makeself(im)
+
+# polar light palette
+palettedata = [46, 52, 64, 59, 66, 82, 67, 76, 94, 76, 86, 106]
+palimage = Image.new('P', (16, 16))
+palimage.putpalette(palettedata * 64)
+oldimage = Image.open("./test.png")
+newimage = quantizetopalette(oldimage, palimage, dither=False)
+newimage.save('wat.png')
