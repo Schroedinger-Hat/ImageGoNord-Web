2497566ba7c5cc4a35095854fa4e82137e5ed596
Wabri <gabriele.puliti@protonmail.com>
2020-07-21 18:49:28 +0200

    Refactor main script: import palette and create tripletes
    3 new methods:
      * export tripletes from color
      * import palette  from file
      * create palette data => allow to create the dataset for the image

--- src/quantize-palette.py
+++ src/quantize-palette.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from PIL import Image
 
+palettedata = create_palette_data(
+    import_palette_from_file("palettes/Frost.txt"))
 class NordPalette():
     POLAR_NIGHT = [46, 52, 64, 59, 66, 82, 67, 76, 94, 76, 86, 106]
     SNOW_STORM = [216, 222, 233, 229, 233, 240, 236, 239, 244]
@@ -30,7 +34,29 @@ def quantizetopalette(silf, palette, dither=False):
     # retrocompatibilità per le 4.0 di pillow
     return im2
 
-# polar light palette
+
+def export_tripletes_from_color(hex_color):
+    hex_triplets = [hex_color[i:i+2] for i in range(0, len(hex_color), 2)]
+    triplets_integer = [int(hex_triplets[i], 16)
+                        for i in range(len(hex_triplets))]
+    return triplets_integer
+
+
+def import_palette_from_file(filename):
+    f = open(filename, "r")
+    palette = [line.replace('#', '').replace('\n', '')
+               for line in f.readlines()]
+    palette.remove('')
+    return palette
+
+
+def create_palette_data(palette):
+    data = []
+    for color in palette:
+        data.extend((export_tripletes_from_color(color)))
+    return data
+
+
 palimage = Image.new('P', (16, 16))
 palimage.putpalette(palettedata * 16)
 oldimage = Image.open("images/mountain.jpg")
