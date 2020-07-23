757edf6790a70ae208299d647096d9a2f91492dd
thejoin95 <miki.lombi@gmail.com>
2020-07-23 16:46:45 +0200

    fix tabs indent

--- src/replace-color.py
+++ src/replace-color.py
@@ -7,28 +7,28 @@ USE_AVG_COLOR = True
 USE_GAUSSIAN_BLUR = False
 
 def export_tripletes_from_color(hex_color):
-    hex_triplets = [hex_color[i:i+2] for i in range(0, len(hex_color), 2)]
-    triplets_integer = [int(hex_triplets[i], 16)
-                        for i in range(len(hex_triplets))]
-    return triplets_integer
+  hex_triplets = [hex_color[i:i+2] for i in range(0, len(hex_color), 2)]
+  triplets_integer = [int(hex_triplets[i], 16)
+                      for i in range(len(hex_triplets))]
+  return triplets_integer
 
 
 def import_palette_from_file(filename):
-    f = open(filename, "r")
-    palette = [line.replace('#', '').replace('\n', '')
-               for line in f.readlines()]
-    palette.remove('')
-    return palette
+  f = open(filename, "r")
+  palette = [line.replace('#', '').replace('\n', '')
+              for line in f.readlines()]
+  palette.remove('')
+  return palette
 
 
 def create_palette_data(palette):
-    data = []
-    for color in palette:
-        data.extend((export_tripletes_from_color(color)))
-    return data
+  data = []
+  for color in palette:
+      data.extend((export_tripletes_from_color(color)))
+  return data
 
 def color_difference (color1, color2):
-    return sum([abs(component1-component2) for component1, component2 in zip(color1, color2)])
+  return sum([abs(component1-component2) for component1, component2 in zip(color1, color2)])
 
 def get_avg_color (pixels, w=-2, h=3):
   average_sum = []
