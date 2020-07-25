8926c45b5fc6f8160aa24d46e8820ae26abb2711
Wabri <gabriele.puliti@protonmail.com>
2020-07-25 18:16:26 +0200

    Update quantize-palette.py: define colors and image arguments
    now is possible to give the colors name and image path

--- src/quantize-palette.py
+++ src/quantize-palette.py
@@ -8,9 +8,10 @@ This module does stuff.
 import sys
 from os import path
 from signal import signal, SIGINT
-from utility.palette_loader import find_palette, load_palette_set
+from utility.palette_loader import find_palettes, load_palette_set
 from PIL import Image, ImageFilter
 
+BLACK_REPLACE = '2E3440'
 
 def quantizetopalette(silf, palette):
     """<Short Description>
@@ -100,7 +101,7 @@ def import_palette_from_file(filename):
     return palette
 
 
-def create_palette_data(palette):
+def create_data_colors(palette):
     """<Short Description>
 
       <Description>
@@ -141,54 +142,83 @@ def signal_handler(self, sig, frame):
     sys.exit(0)
 
 
+def is_colors_selected(selection, color_name):
+    """<Short Description>
+
+      <Description>
+
+    Parameters
+    ----------
+    <argument name>: <type>
+      <argument description>
+    <argument>: <type>
+      <argument description>
+
+    Returns
+    -------
+    <type>
+      <description>
+    """
+    for index in range(len(selection)):
+        if selection[index] != color_name[index].lower():
+            return False
+    return True
+
+
 if __name__ == '__main__':
     signal(SIGINT, signal_handler)
     args = sys.argv[1:]
 
-    # Get absolute path of source
+    # Get absolute path of source project
     path = path.dirname(path.realpath(__file__))
 
     # Get all palettes created
-    palettes = find_palette(path + "/palettes")
+    palettes = find_palettes(path + "/palettes")
 
     for arg in args:
-
-        # list_palette = load_palette(path)
 
         key_value = arg.split("=")
 
         palettedata = []
 
         for palette in palettes:
+            palette_path = path + "/palettes/" + palette.capitalize() + "/"
             if "--{}".format(palette) in key_value[0].lower():
                 if len(key_value) == 1:
                     print("Use all {} color set".format(palette.capitalize()))
-                    palette_path = path + "/palettes/" + palette.capitalize() + "/"
                     palette_set = load_palette_set(palette_path)
-                    for colors in palette_set:
-                        palettedata.extend(create_palette_data(
-                            import_palette_from_file(palette_path + colors + ".txt")))
+                    for colors_name in palette_set:
+                        colors_palette = import_palette_from_file(
+                            palette_path + colors_name + ".txt")
+                        colors_set = create_data_colors(colors_palette)
+                        palettedata.extend(colors_set)
                 else:
-                    print("TODO")
+                    print("Use {} palette".format(palette.capitalize()))
+                    selected_colors = key_value[1].split(",")
+                    palette_set = load_palette_set(palette_path)
+                    for selected_color_set in selected_colors:
+                        for colors_name in palette_set:
+                            if is_colors_selected(selected_color_set, colors_name):
+                                print("Selected {} as {}".format(selected_color_set, colors_name))
+                                colors_palette = import_palette_from_file(
+                                    palette_path + colors_name + ".txt")
+                                colors_set = create_data_colors(colors_palette)
+                                palettedata.extend(colors_set)
+                    if len(palettedata) == 0:
+                        print("No colors set correctly given!")
+                        print("Exit!")
 
-        print(palettedata)
-
-        sys.exit(0)
-
-        if not is_all:
-            for palette in list_palette:
-                if '--' + palette.lower() in key_value[0]:
-                    print("Use {} color set".format(palette))
-                    palettedata.extend(create_palette_data(
-                        import_palette_from_file(path+palette)))
+        if key_value[0].lower() == "--img" and len(key_value) > 1:
+            input_image = key_value[1]
+            print("Load image {}".format(path + "/" + input_image))
 
     sys.exit(0)
 
-   # padding with black color | nordtheme palette is only 48
-while len(palettedata) < 768:
-    palettedata.extend(export_tripletes_from_color('2E3440'))
-
-palimage = Image.new('P', (1, 1))
-palimage.putpalette(palettedata)
-oldimage = Image.open("images/lui.jpg")
-quantizetopalette(oldimage, palimage)
+    # padding with black color | nordtheme palette is only 48
+    while len(palettedata) < 768:
+        palettedata.extend(export_tripletes_from_color(BLACK_REPLACE))
+    
+    palimage = Image.new('P', (1, 1))
+    palimage.putpalette(palettedata)
+    oldimage = Image.open("images/lui.jpg")
+    quantizetopalette(oldimage, palimage)
