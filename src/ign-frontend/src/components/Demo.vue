7deba4b13e9a5ae45979608538395b15209ecc3f
thejoin95 <miki.lombi@gmail.com>
2020-08-05 16:23:17 +0200

    add img upload and conversion to canvas

--- src/ign-frontend/src/components/Demo.vue
+++ src/ign-frontend/src/components/Demo.vue
@@ -2,7 +2,7 @@
   <div class="demo container">
     <input type="file" accept="image/*" id="file" @change="loadFile" style="display:none;">
     <p><label for="file" style="cursor: pointer;">Upload Image</label></p>
-    <canvas width="450" height="350" id="img-preview"></canvas>
+    <canvas width="450" height="450" id="img-preview"></canvas>
   </div>
 </template>
 
@@ -14,11 +14,13 @@ export default Vue.component('Demo', {
   methods: {
     loadFile: (event) => {
       console.log(event);
+      const canvas = document.getElementById('img-preview');
       const ctx = document.getElementById('img-preview').getContext('2d');
       const img = new Image();
 
       img.onload = () => {
-        ctx.drawImage(img, 0, 0);
+        ctx.drawImage(img, 0, 0, img.width, img.height,
+          0, 0, canvas.width, canvas.height);
       };
 
       img.src = URL.createObjectURL(event.target.files[0]);
