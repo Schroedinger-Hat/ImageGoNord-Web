4b81e607b7df095ae3f1e3c651932a310772b8eb
Wabri <gabriele.puliti@protonmail.com>
2020-08-08 17:14:56 +0200

    Refactor cli: set up image detection and refactor :)

--- src/cli.py
+++ src/cli.py
@@ -81,7 +81,7 @@ def is_colors_selected(selection, color_name):
     return True
 
 
-def log(string):
+def to_console(string):
     """<Short Description>
 
       <Description>
@@ -140,6 +140,17 @@ if __name__ == '__main__':
         print(VERSION)
         sys.exit(0)
 
+    IMAGE_ARGUMENT_PATTERN = r'-[-img|i]=*'
+    IS_IMAGE_PASSED = False
+    for arg in args:
+        searched_arg = re.search(IMAGE_ARGUMENT_PATTERN, arg)
+        if searched_arg is not None:
+            IS_IMAGE_PASSED = True
+            break
+    if not IS_IMAGE_PASSED:
+        print("The image path must be given!")
+        sys.exit(1)
+
     QUIET_MODE = "-q" in args or "--quiet" in args
 
     # Get absolute path of source project
@@ -158,9 +169,10 @@ if __name__ == '__main__':
         if condition_argument:
             if len(key_value) > 1 and (re.search(IMAGE_PATTERN, key_value[1]) is not None):
                 input_image = key_value[1]
-                log("Load image {}".format(src_path + "/" + input_image))
+                to_console("Load image {}".format(
+                    src_path + "/" + input_image))
             else:
-                log("You need to pass the image path!")
+                to_console("You need to pass the image path!")
                 sys.exit(1)
             continue
 
@@ -171,19 +183,19 @@ if __name__ == '__main__':
                 # If the image name have already an extension do not set the default one
                 output_image_name += "" if re.search(
                     IMAGE_PATTERN, output_image_name) else DEAFAULT_EXTENSION
-                log("Output image {}".format(
+                to_console("Output image {}".format(
                     src_path + "/" + output_image_name))
             else:
-                log("No output filename specify within the arguments!")
+                to_console("No output filename specify within the arguments!")
             continue
 
         condition_argument = key in ("--no_average" or "-n")
         if condition_argument:
             if not len(key_value) > 1:
                 no_average = True
-                log("No average pixels selected!")
+                to_console("No average pixels selected!")
             else:
-                log("No average pixels do not want any values!")
+                to_console("No average pixels do not want any values!")
             continue
 
         condition_argument = key in ("--pixels_area" or "-p")
@@ -191,16 +203,16 @@ if __name__ == '__main__':
             if len(key_value) > 1:
                 try:
                     pixels = int(key_value[1])
-                    log("The area in pixels is {}".format(pixels))
+                    to_console("The area in pixels is {}".format(pixels))
                 except ValueError as value_error:
-                    log("The area pixels must be a number value!")
+                    to_console("The area pixels must be a number value!")
                     sys.exit(1)
             else:
-                log("To set the area pixels you must pass a number!")
+                to_console("To set the area pixels you must pass a number!")
             continue
 
         del condition_argument
-    
+
         palettedata = []
 
         for palette in palettes:
@@ -218,31 +230,29 @@ if __name__ == '__main__':
                         colors_set = create_data_colors(colors_palette)
                         palettedata.extend(colors_set)
                 else:
-                    log("Use {} palette".format(palette.capitalize()))
+                    to_console("Use {} palette".format(palette.capitalize()))
                     selected_colors = key_value[1].split(",")
                     palette_set = load_palette_set(palette_path)
                     for selected_color_set in selected_colors:
                         for colors_name in palette_set:
                             if is_colors_selected(selected_color_set, colors_name):
-                                log("Selected {} as {}".format(
+                                to_console("Selected {} as {}".format(
                                     selected_color_set, colors_name))
                                 colors_palette = import_palette_from_file(
                                     palette_path + colors_name + ".txt")
                                 colors_set = create_data_colors(colors_palette)
                                 palettedata.extend(colors_set)
                     if len(palettedata) == 0:
-                        log("No colors set correctly given!")
-                        log("Exit!")
-
-        
+                        to_console("No colors set correctly given!")
+                        to_console("Exit!")
 
     sys.exit(0)
 
     # padding with black color | nordtheme palette is only 48
-    while len(palettedata) < 768:
-        palettedata.extend(export_tripletes_from_color(BLACK_REPLACE))
+    # while len(palettedata) < 768:
+    #    palettedata.extend(export_tripletes_from_color(BLACK_REPLACE))
 
-    palimage = Image.new('P', (1, 1))
-    palimage.putpalette(palettedata)
-    oldimage = Image.open("images/lui.jpg")
-    quantizetopalette(oldimage, palimage)
+    #palimage = Image.new('P', (1, 1))
+    # palimage.putpalette(palettedata)
+    #oldimage = Image.open("images/lui.jpg")
+    #quantizetopalette(oldimage, palimage)
