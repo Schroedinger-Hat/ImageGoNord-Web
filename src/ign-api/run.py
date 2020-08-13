0bb40601902956998eddc7db327cff832df75100
thejoin95 <miki.lombi@gmail.com>
2020-08-13 18:31:57 +0200

    initialize quantize and convert endpoint in the same way

--- src/ign-api/run.py
+++ src/ign-api/run.py
@@ -20,23 +20,85 @@ def get_api_status():
 @app.route(API_VERSION + "/quantize", methods=["POST"])
 @cross_origin()
 def quantize():
-  return jsonify({'ok': True})
+  go_nord = setup_instance(request)
+  output_path = ''
+  response = {'success': True}
+
+  if (request.files.get('file') != None):
+    image = go_nord.open_image(request.files.get('file').stream)
+  elif (request.form.get('file_path') != None):
+    image = go_nord.open_image(request.form.get('file_path'))
+  elif (request.form.get('b64_input') != None):
+    image = go_nord.base64_to_image(request.form.get('b64_input'))
+  else:
+    abort(400, 'You need to provide at least a valid image or image path')
+
+  if (request.form.get('width') and request.form.get('height')):
+    image = go_nord.resize_image(image)
+
+  if (request.form.get('output_path') != None):
+    output_path = request.form.get('output_path')
+
+  image = go_nord.quantize_image(image, save_path=output_path)
+  
+  if (request.form.get('b64_output') != None):
+    b64_image = go_nord.image_to_base64(image, 'jpeg')
+    response['b64_img'] = b64_image
+  
+  return jsonify(response)
 
 @app.route(API_VERSION + "/convert", methods=["POST"])
 @cross_origin()
 def convert():
-  # abort(400, "You need to provide at least")
-  try:
-    f = request.files['file']
-    # request.files['file'].stream
-    print(f)
-    print(f.filename)
-  except Exception as e:
-    print(e)
-    print(request.data)
-    pass
-  # f.save(secure_filename(f.filename))
-  return jsonify({'ok': True})  
+  go_nord = setup_instance(request)
+  output_path = ''
+  response = {'success': True}
+
+  if (request.files.get('file') != None):
+    image = go_nord.open_image(request.files.get('file').stream)
+  elif (request.form.get('file_path') != None):
+    image = go_nord.open_image(request.form.get('file_path'))
+  elif (request.form.get('b64_input') != None):
+    image = go_nord.base64_to_image(request.form.get('b64_input'))
+  else:
+    abort(400, 'You need to provide at least a valid image or image path')
+
+  if (request.form.get('width') and request.form.get('height')):
+    image = go_nord.resize_image(image)
+
+  if (request.form.get('output_path') != None):
+    output_path = request.form.get('output_path')
+
+  image = go_nord.convert_image(image, save_path=output_path)
+  
+  if (request.form.get('b64_output') != None):
+    b64_image = go_nord.image_to_base64(image, 'jpeg')
+    response['b64_img'] = b64_image
+  
+  return jsonify(response)
+
+def setup_instance(req):
+  go_nord = GoNord()
+
+  hex_colors = []
+  if (req.form.get('colors') != None):
+    hex_colors = req.form.get('colors').split(',')
+
+  if (len(hex_colors) > 0):
+    go_nord.reset_palette()
+    for hex_color in hex_colors:
+      go_nord.add_color_to_palette(hex_color)
+
+  if (req.form.get('is_avg') != None):
+    go_nord.enable_avg_algorithm()
+
+  if (req.form.get('avg_box_width') != None and req.form.get('avg_box_height') != None):
+    go_nord.set_avg_box_data(int(req.form.get('avg_box_width')), int(req.form.get('avg_box_height')))
+
+  if (req.form.get('blur') != None):
+    go_nord.enable_gaussian_blur()
+
+  return go_nord
 
 if __name__ == '__main__':
 	app.run(port=8000, threaded=True)
