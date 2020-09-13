a7997869cd6a9fccf2d7a57bd7ebc16c766ef506
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2020-09-13 21:18:49 +0200

    Starting refactoring

--- src/ign-api/run.py
+++ src/ign-api/run.py
@@ -8,8 +8,8 @@ from ImageGoNord import GoNord, NordPaletteFile
 API_VERSION = '/v1'
 
 app = Flask(__name__)
-
 cors = CORS(app)
+
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
@@ -26,24 +26,24 @@ def quantize():
     output_path = ''
     response = {'success': True}
 
-    if (request.files.get('file') != None):
+    if request.files.get('file') != None:
         image = go_nord.open_image(request.files.get('file').stream)
-    elif (request.form.get('file_path') != None):
+    elif request.form.get('file_path') != None:
         image = go_nord.open_image(request.form.get('file_path'))
-    elif (request.form.get('b64_input') != None):
+    elif request.form.get('b64_input') != None:
         image = go_nord.base64_to_image(request.form.get('b64_input'))
     else:
         abort(400, 'You need to provide at least a valid image or image path')
 
-    if (request.form.get('width') and request.form.get('height')):
+    if request.form.get('width') and request.form.get('height'):
         image = go_nord.resize_image(image)
 
-    if (request.form.get('output_path') != None):
+    if request.form.get('output_path') != None:
         output_path = request.form.get('output_path')
 
     image = go_nord.quantize_image(image, save_path=output_path)
 
-    if (request.form.get('b64_output') != None):
+    if request.form.get('b64_output') != None:
         b64_image = go_nord.image_to_base64(image, 'jpeg')
         base64_img_string = b64_image.decode('UTF-8')
         response['b64_img'] = base64_img_string
@@ -58,24 +58,24 @@ def convert():
     output_path = ''
     response = {'success': True}
 
-    if (request.files.get('file') != None):
+    if request.files.get('file') != None:
         image = go_nord.open_image(request.files.get('file').stream)
-    elif (request.form.get('file_path') != None):
+    elif request.form.get('file_path') != None:
         image = go_nord.open_image(request.form.get('file_path'))
-    elif (request.form.get('b64_input') != None):
+    elif request.form.get('b64_input') != None:
         image = go_nord.base64_to_image(request.form.get('b64_input'))
     else:
         abort(400, 'You need to provide at least a valid image or image path')
 
-    if (request.form.get('width') and request.form.get('height')):
+    if request.form.get('width') and request.form.get('height'):
         image = go_nord.resize_image(image)
 
-    if (request.form.get('output_path') != None):
+    if request.form.get('output_path') != None:
         output_path = request.form.get('output_path')
 
     image = go_nord.convert_image(image, save_path=output_path)
 
-    if (request.form.get('b64_output') != None):
+    if request.form.get('b64_output') != None:
         b64_image = go_nord.image_to_base64(image, 'jpeg')
         base64_img_string = b64_image.decode('UTF-8')
         response['b64_img'] = base64_img_string
@@ -87,21 +87,21 @@ def setup_instance(req):
     go_nord = GoNord()
 
     hex_colors = []
-    if (req.form.get('colors') != None):
+    if req.form.get('colors') != None:
         hex_colors = req.form.get('colors').split(',')
 
-    if (len(hex_colors) > 0):
+    if len(hex_colors) > 0:
         go_nord.reset_palette()
         for hex_color in hex_colors:
             go_nord.add_color_to_palette(hex_color)
 
-    if (req.form.get('is_avg') != None):
+    if req.form.get('is_avg') != None:
         go_nord.enable_avg_algorithm()
 
-    if (req.form.get('avg_box_width') != None and req.form.get('avg_box_height') != None):
+    if req.form.get('avg_box_width') != None and req.form.get('avg_box_height') != None:
         go_nord.set_avg_box_data(int(req.form.get('avg_box_width')), int(req.form.get('avg_box_height')))
 
-    if (req.form.get('blur') != None):
+    if req.form.get('blur') != None:
         go_nord.enable_gaussian_blur()
 
     return go_nord
